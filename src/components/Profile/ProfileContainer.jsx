0012f6700398e6b45832dea424539df779024dbd
gleb_zinchenko <gleb.z164@gmail.com>
2020-09-04 14:31:03 +0300

    profileDescription fix

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -9,7 +9,7 @@ import { compose } from 'redux';
 
 class ProfileContainer extends React.Component {
     
-    componentDidMount () {
+    refreshProfile() {
         let userId = this.props.match.params.userId || this.props.userId;
         if (!userId) {
             userId = this.props.userId;
@@ -20,6 +20,16 @@ class ProfileContainer extends React.Component {
         
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
+    }
+
+    componentDidMount() {
+        this.refreshProfile();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.match.params.userId != prevProps.match.params.userId) {
+            this.refreshProfile();
+        }   
     }
     
     render () {
--- src/components/Profile/ProfileDescription/ProfileDescription.jsx
+++ src/components/Profile/ProfileDescription/ProfileDescription.jsx
@@ -14,7 +14,7 @@ const ProfileDescription = ({profile, status, updateStatus}) => {
             <div>
                 <img className={classes.img} src='https://image.shutterstock.com/z/stock-photo-wide-angle-panorama-autumn-forest-misty-hills-mountain-tops-in-pink-dawn-1195159864.jpg' />
             </div>
-            <img className={classes.avatar} src={profile.photos.large !== null ? profile.photos.large : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTiuNTmUWSMIV5d4jUHP_UgIpTZHt7bElWqAw&usqp=CAU'} />
+            <img className={classes.avatar} src={profile.photos.large || 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTiuNTmUWSMIV5d4jUHP_UgIpTZHt7bElWqAw&usqp=CAU'} />
             <h1>{profile.fullName}</h1>
             <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
 
